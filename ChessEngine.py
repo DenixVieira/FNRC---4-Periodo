ef6b6867d238d8eb81e63cb60f5639843075d07e
Denilson Vieira <mcdenixson@gmail.com>
2023-09-22 06:53:53 -0300

    Adicionando os movimentos das peças

--- ChessEngine.py
+++ ChessEngine.py
@@ -6,7 +6,7 @@ class GameState():
     def __init__(self) -> None:
 
         # A tábua é possui lista de 8x8 e cada casa possui um elemento de dois caractere
-        # O primeiro caractere representa a cor da peça "W" para white e "B" para Black
+        # O primeiro caractere representa a cor da peça "W" para white(Branco) e "B" para Black(Preto)
         # O segundo caractere representa o tipo da peça nas seguinte ordem: Torre, Cavalo, Bispo, Rainha, Rei, Bispo, Cavalo, Torre
         # '--' significa espaço vazio
 
@@ -18,8 +18,46 @@ class GameState():
             ['--', '--', '--', '--', '--', '--', '--', '--'],
             ['--', '--', '--', '--', '--', '--', '--', '--'],
             ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP'],
-            ['wT', 'wC', 'wB', 'wR', 'wK', 'wB', 'wC', 'wT'],
+            ['wT', 'wC', 'wB', 'wK', 'wR', 'wB', 'wC', 'wT'],
         ]
 
         self.whiteToMove = True
         self.moveLog = []
+
+    def makemove(self, move):
+        self.board[move.startRow][move.startCol] = '--'
+        self.board[move.endRow][move.endCol] = move.pieceMoved
+        # armazenando os movimentos gerados pelos jogadores
+        self.moveLog.append(move)
+        self.whiteToMove = not self.whiteToMove  # trocar os jogadores
+
+    class Move():
+
+        # Numerando as linhas e colunas de 1 a 8
+        ranksToRows = {"1": 7, "2": 6, "3": 5, "4": 4,
+                       "5": 3, "6": 2, "7": 1, "1": 0}
+        rowsToRank = {v:k for k, v in ranksToRows.items()}
+
+        # Mapeando as colunas de A a H
+        filesToCols = {"a": 0, "b": 1, "c": 2, "d": 3,
+                       "e": 4, "f": 5, "g": 6, "h": 7}
+        colsToFile = {v:k for k, v in filesToCols.items()}
+
+        def __init__(self, startSq, endSq, board):
+
+            self.startRow = startSq[0]
+            self.startCol = startSq[1]
+            self.endRow = endSq[0]
+            self.endCol = endSq[1]
+
+            # Onde a peça estava
+            self.pieceMoved = board[self.startRow][self.startCol]
+            # Onde a peça será movida
+            self.pieceCaptured = board[self.endRow][self.endCol]
+
+        def getChessNotation(self):
+            # Essa função retorna a posição da casa e da coluna
+            return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endRow)
+
+        def getRankFile(self, r, c):
+            return self.colsToFile[c] + self.rowsToRank[r]
--- ChessMain.py
+++ ChessMain.py
@@ -1,4 +1,4 @@
-''' Esse é o arquivo principal, ele será repsonssável por receber os comandos no display que ocorrer durante o jogo '''
+''' Esse é o arquivo principal, ele será responssável por receber os comandos no display que ocorrer durante o jogo '''
 
 import pygame as p
 from ChessEngine import *
@@ -8,6 +8,7 @@ DIMENSION = 8  # Tamanho da Tábua é 8x8
 SQ_SIZE = HEIGHT // DIMENSION  # O Tamanho do quadrado será responsivo à resolução
 MAX_FPS = 15  # Para futuras animações
 IMAGES = {}
+
 
 '''
 Para melhor desempenho, é necessário que as imagens sejam alocadas na memória apenas uma vez no arquivo principal
@@ -32,6 +33,7 @@ O arquivo principal do projeto, ele irá dar conta da rederização e atualizaç
 
 
 def main():
+
     p.init()
     screen = p.display.set_mode((WIDTH, HEIGHT))  # Criando a tela do jogo
     clock = p.time.Clock()  # Relógio do jogo
@@ -39,10 +41,41 @@ def main():
     gs = GameState()
     loadImages()  # As imagens é chamada apenas uma vez antes do loop
     running = True  # condição para o loop da tela
+    sqSelected = ()  # Armazena a casa que o usuário clicar, (Tupla: (linha, coluna))
+    # mantem armazenado os clicks do usuário (duas tulplas: [(6,4),(4,4)])
+    playerClicks = []
+
     while running:
-        for e in p.event.get():  # Evento de sair
-            if e.type == p.QUIT:
+
+        for e in p.event.get():
+
+            if e.type == p.QUIT:  # Evento de sair
                 running = False
+
+            elif e.type == p.MOUSEBUTTONDOWN:  # Evento de click do mouse
+                location = p.mouse.get_pos()  # captura a posição do mouse
+                col = location[0]//SQ_SIZE
+                row = location[1]//SQ_SIZE
+
+                if sqSelected == (row, col):  # o usuário clickou nas mesmas casas
+                    sqSelected = ()
+                    playerClicks = []  # retira o click do player
+
+                else:
+                    sqSelected = (row, col)
+                    # Adicionando o primeiro e segundo click do usuário
+                    playerClicks.append(sqSelected)
+
+                if len(playerClicks) == 2:
+                    if gs.board[playerClicks[0][0]][playerClicks[0][1]] == '--':
+                        sqSelected = ()
+                        playerClicks = []
+                    else:
+                        move = gs.Move(
+                            playerClicks[0], playerClicks[1], gs.board)
+                        gs.makemove(move)
+                        sqSelected = ()  # reseta a seleção
+                        playerClicks = []  # resetando os clicks
 
         drawGameState(screen, gs)
         clock.tick(MAX_FPS)
@@ -55,12 +88,12 @@ Responsável por todos os desenhos com e interações
 
 
 def drawGameState(screen, gs):
-    drawBoard(screen)  # Desenha os quadrados na tábua
+    drawBoard(screen)
     drawPieces(screen, gs.board)
 
 
 '''
-#Desenha os quadrados na tábua
+# Desenha os quadrados na tábua
 '''
 
 
@@ -70,8 +103,8 @@ def drawBoard(screen):
         for c in range(DIMENSION):
 
             '''
-            levando em consideração que o r na posição 0 e o r na posição 1 irão dar restos diferentes quando forem 
-            comparado com o c na posição zero por exemplo. Então abaixo temos a seleção das cores mediante o que for o 
+            levando em consideração que o r(linha) na posição 0 e o r na posição 1 irão dar restos diferentes quando forem 
+            comparado com o c(coluna) na posição zero por exemplo. Então abaixo temos a seleção das cores mediante o que for o 
             resultado do resto
             '''
 
@@ -86,6 +119,7 @@ desenha as peças em cima dos quadrados
 
 
 def drawPieces(screen, board):
+
     for r in range(DIMENSION):
         for c in range(DIMENSION):
             piece = board[r][c]
